0b863d9d038c55167834bcd9eee33f8239297a88
brunogomespascotto <panoramic56@proton.me>
2024-08-12 22:22:15 -0700

    Initial commit

---
+++ DiagramsScript.js
@@ -0,0 +1,23 @@
+// require("./main.css");
+// import "./styles.css";
+
+document.querySelectorAll('.ChartDiv').forEach(box => {
+    console.log("A");
+    let hideTimeout;
+    box.addEventListener('mouseover', () => {
+        hideTimeout = setTimeout(() => {
+            document.querySelectorAll('.ChartDiv').forEach(otherBox => {
+                if (otherBox !== box) {
+                    otherBox.classList.add('hidden');
+                }
+            });
+        }, 1000); // 500ms delay before hiding other divs
+    });
+
+    box.addEventListener('mouseout', () => {
+        clearTimeout(hideTimeout);
+        document.querySelectorAll('.ChartDiv').forEach(otherBox => {
+            otherBox.classList.remove('hidden');
+        }, 1000);
+    });
+});
